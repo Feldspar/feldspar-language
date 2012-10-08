87600dc140158a99175564c1411c5d8f048fd76c
Anders Persson <anders.cj.persson@gmail.com>
2012-10-08 12:59:17 +0200

    Remove redundant pragma

--- src/Feldspar/Core/Constructs/Array.hs
+++ src/Feldspar/Core/Constructs/Array.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
