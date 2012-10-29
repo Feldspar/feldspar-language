6e553dce1b13937b27058abebf51bfd7d7abb005
Emil Axelsson <emax@chalmers.se>
2012-10-29 16:05:55 +0100

    Added test suite for tutorial

---
+++ tests/TutorialTest.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Tutorial.Basic
+import Tutorial.Array
+import Tutorial.Vector
+import Tutorial.Size
+import Tutorial.Patch
+
+-- Currently only testing compilation
+
+main = return ()
+
