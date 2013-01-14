c500f2987d3cf6c7f0b51878b47c262784b9cb68
Anders Persson <anders.cj.persson@gmail.com>
2013-01-14 14:07:06 +0100

    Update range propagation test case

--- tests/DecorationTests.hs
+++ tests/DecorationTests.hs
@@ -1,7 +1,7 @@
 module Main where
 
 -- To generate the golden files use a script similiar to this one
--- > ghc -e 'B.writeFile "tests/gold/example9.txt" $ B.pack $ showDecor example9' tests/DecorationTests.hs -iexamples
+-- > ghc -isrc -iexampes -itests -e 'B.writeFile "tests/gold/example9.txt" $ B.pack $ showDecor example9' tests/DecorationTests.hs -iexamples
 
 import Test.Framework
 import Test.Golden
@@ -12,7 +12,7 @@ import Feldspar (showDecor)
 import Examples.Simple.Basics
 
 tests = testGroup "DecorationTests"
-    [ goldenVsString "example9" "tests/gold/example9.txt" $ return $ B.pack $ showDecor example9
+    [ goldenVsFile "example9" "tests/gold/example9.txt" "tests/example9.txt" $ B.writeFile "tests/example9.txt" $ B.pack $ showDecor example9
     ]
 
 main = defaultMain [tests]
