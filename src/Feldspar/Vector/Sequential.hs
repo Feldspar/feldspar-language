2c1804b96c110b53c3f693fbb48d78e56dfb6ad4
Josef Svenningsson <josef.svenningsson@gmail.com>
2013-06-23 00:02:39 +0200

    Use update-in-place in sequential vectors for efficiency

--- src/Feldspar/Vector/Sequential.hs
+++ src/Feldspar/Vector/Sequential.hs
@@ -1,37 +1,74 @@
+{-# LANGUAGE GADTs #-}
 module Feldspar.Vector.Sequential where
 
+import qualified Prelude
 import Feldspar
 
 data Seq a where
-  Seq :: Syntactic s => Data Length -> s -> (Data Index -> s -> (a,s)) -> Seq a
+  Seq :: Syntax state => Data Length -> M state -> (state -> M a) -> Seq a
 
 length :: Seq a -> Data Length
 length (Seq l _ _) = l
 
-toArray :: Seq (Data a) -> Data [a]
-toArray (Seq l init next) = sequential l init next
+toArray :: Type a => Seq (Data a) -> Data [a]
+toArray (Seq l init next) = runMutableArray $
+                            do state <- init
+                               arr   <- newArr_ l
+                               forM l (\i -> do
+                                             a <- next state
+                                             setArr arr i a
+                                      )
+                               return arr
 
 instance Functor Seq where
-  fmap f (Seq l i next) = Seq l i (\i s -> let (a,s') = next i s
-                                           in (f a,s'))
+  fmap f (Seq l i next) = Seq l i n
+    where
+      n s = do a <- next s
+               return (f a)
 
-seq :: Syntactic s => Data Length -> s -> (Data Index -> s -> (a,s)) -> Seq s
-seq l i next = Seq l i next
+tail :: Syntax a => Seq a -> Seq a
+tail (Seq l i next) = Seq (max 0 (l-1)) i' next
+  where i' = do s <- i
+                next s
+                return s
 
+seq :: Syntax s => Data Length -> s -> (Data Index -> s -> (a,s)) -> Seq a
+seq l i next = Seq l init step
+  where
+    init = do rc <- newRef (0 :: Data WordN)
+              rs <- newRef i
+              return (rs,rc)
+    step (rs,rc) = do c <- getRef rc
+                      s <- getRef rs
+                      let (a,s') = next c s
+                      setRef rs s'
+                      setRef rc (c+1)
+                      return a
 
--- Return a sequence of numbers between -0.5 and 0.5 to indicate 
-demod :: Data Float -> Data WordN -> Seq (Data Float)
-demod c i = seq i c next
-  where next _ c = let a = clamp c
-                   in (a, if a >= 0 then (a - 0.5) * 2 else (a + 0.5) * 2)
+zipWith :: (a -> b -> c) -> Seq a -> Seq b -> Seq c
+zipWith f (Seq l1 i1 n1) (Seq l2 i2 n2) = Seq (min l1 l2) i next
+  where
+    i = do s1 <- i1
+           s2 <- i2
+           return (s1,s2)
+    next (s1,s2) = do a <- n1 s1
+                      b <- n2 s2
+                      return (f a b)
 
-interleave :: Seq a -> Seq a -> Seq a
+interleave :: Syntax a => Seq a -> Seq a -> Seq a
 interleave (Seq l1 i1 n1) (Seq l2 i2 n2)
-  = Seq (l1+l2) (true,i1,0,i2,0) next
-  where next i (b,s1,i1,s2,i2) = b ? (let (a,s1') = n1 i1 s1
-  	       		       	      in (a,(false,s1',i1+1,s2,i2))
-				     ,let (a,s2') = n2 i2 s2
-				      in (a,(true,s1,i1,s2',i2+1)))
-
-qam64 :: Data (Complex Float) -> Seq (Data Float)
-qam64 c = interleave (demod (real c)) (demod (imag c))
+  = Seq (2*(min l1 l2)) i next
+  where
+    i = do s1 <- i1
+           s2 <- i2
+           b  <- newRef true
+           return (b,s1,s2)
+    next (rb,s1,s2) = do b <- getRef rb
+                         setRef rb (not b)
+                         ifM b
+                           (n1 s1)
+                           (n2 s2)
+-- This function is slower than need be, because we can only write one
+-- element at a time. If we had something akin to a push array then we
+-- wouldn't have to have a boolean to switch between the two different
+-- step functions, we could just execute them one after the other.
