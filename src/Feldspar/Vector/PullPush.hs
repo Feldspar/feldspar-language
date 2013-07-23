3292fb7858bfdff2e15e35bd2cd626b1d25a5574
Josef Svenningsson <josef.svenningsson@gmail.com>
2013-07-23 22:59:14 +0200

    Fix the types in Feldspar.Vector.PullPush

--- src/Feldspar/Vector/PullPush.hs
+++ src/Feldspar/Vector/PullPush.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 module Feldspar.Vector.PullPush where
 
 import qualified Prelude as P
@@ -12,46 +13,46 @@ data Vector a where
 
 -- Internal
 
-toPush :: Vector a -> Ph.PushVector a
+toPush :: Syntax a => Vector a -> Ph.PushVector a
 toPush (Pull pull) = Ph.toPush pull
 toPush (Push push) = push
 
 -- allocates memory
-pushToPull :: Ph.PushVector a -> Pl.PullVector a
+pushToPull :: Syntax a => Ph.PushVector a -> Pl.PullVector a
 pushToPull push = (Pl.thawPull (Ph.freezePush push))
 
 -- May allocate memory
-toPull :: Vector a -> Pl.PullVector a
+toPull :: Syntax a => Vector a -> Pl.PullVector a
 toPull (Pull pull) = pull
 toPull (Push push) = pushToPull push
 
 -- API
 
 enumFromTo :: Data Index -> Data Index -> Vector (Data Index)
-enumFromTo from to = Pull (Ph.enumFromTo from to)
+enumFromTo from to = Pull (Pl.enumFromTo from to)
 
-index :: Vector a -> Data Index -> a
+index :: Type a => Vector (Data a) -> Data Index -> Data a
 index (Pull pull) ix = pull ! ix
 index (Push push) ix = getIx (Ph.freezePush push) ix
 
-(++) :: Vector a -> Vector a -> Vector a
-vec1 ++ vec2 = toPush vec1 Ph.++ toPush vec2
+(++) :: Syntax a => Vector a -> Vector a -> Vector a
+vec1 ++ vec2 = Push $ toPush vec1 Ph.++ toPush vec2
 
-take :: Data Length -> Vector a -> Vector a
-take n (Pull vec) = Pl.take n vec
-take n (Push vec) = Pl.take n (Pl.thawPull (Ph.freezePush vec))
+take :: Syntax a => Data Length -> Vector a -> Vector a
+take n (Pull vec) = Pull $ Pl.take n vec
+take n (Push vec) = Pull $ Pl.take n (Pl.thawPull (Ph.freezePush vec))
 
-drop :: Data Length -> Vector a -> Vector a
-drop n (Pull vec) = Pl.drop n vec
-drop n (Push vec) = Pl.drop n (Pl.thawPull (Ph.freezePush vec))
+drop :: Syntax a => Data Length -> Vector a -> Vector a
+drop n (Pull vec) = Pull $ Pl.drop n vec
+drop n (Push vec) = Pull $ Pl.drop n (Pl.thawPull (Ph.freezePush vec))
 
-splitAt :: Data Index -> Vector a -> (Vector a, Vector a)
+splitAt :: Syntax a => Data Index -> Vector a -> (Vector a, Vector a)
 splitAt n vec = (take n vec, drop n vec)
 
 permute :: (Data Length -> Data Index -> Data Index) ->
            Vector a -> Vector a
-permute perm (Pull vec) = Pl.permute perm vec
-permute perm (Push vec) = Ph.permute perm vec
+permute perm (Pull vec) = Pull $ Pl.permute perm vec
+permute perm (Push vec) = Push $ Ph.permute perm vec
 
 reverse :: Syntax a => Vector a -> Vector a
 reverse = permute $ \l i -> l-1-i
@@ -65,6 +66,6 @@ rotateVecR ix = reverse . rotateVecL ix . reverse
 replicate :: Data Length -> a -> Vector a
 replicate n a = Pull (Pl.replicate n a)
 
-zip :: Vector a -> Vector b -> Vector (a,b)
-zip v1 v2 = Pl.zip (toPull v1) (toPull v2)
+zip :: (Syntax a,Syntax b) => Vector a -> Vector b -> Vector (a,b)
+zip v1 v2 = Pull $ Pl.zip (toPull v1) (toPull v2)
 
