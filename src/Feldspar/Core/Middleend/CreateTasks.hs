73a7a879f31043c5fd91fb91a1a027a74a61c1e7
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2014-04-14 17:07:43 +0200

    Add support for ParFork to the function creation pass.

--- src/Feldspar/Core/Middleend/CreateTasks.hs
+++ src/Feldspar/Core/Middleend/CreateTasks.hs
@@ -5,6 +5,7 @@ import Control.Monad.State
 import Feldspar.Core.UntypedRepresentation
 
 -- | Create tasks from MkFuture and similar constructs.
+-- Invariant: There are no MkFuture, ParFork or NoInline constructs in the output.
 createTasks :: UntypedFeld -> UntypedFeld
 createTasks e = evalState (go e) 0
 
@@ -16,15 +17,16 @@ go (In (Lambda v e)) = do
 go (In (LetFun (s, k, e1) e2))
  = liftM2 (\e1' e2' -> In (LetFun (s, k, e1') e2')) (go e1) (go e2)
 go l@(In Literal{}) = return l
-go (In (App MkFuture _ [p])) = do
+go (In (App p _ [e])) | p `elem` [MkFuture, ParFork] = do
   p'' <- go p'
   i <- freshId
   let taskName = "task" ++ show i
       core = "task_core" ++ show i
-  return $ In (LetFun (core, Future, p'') (In (App (Call Future taskName) t' vs')))
-   where vs = fv p
+      k = if p == MkFuture then Future else Par
+  return $ In (LetFun (core, k, p'') (In (App (Call k taskName) t' vs')))
+   where vs = fv e
          vs' = map (In . Variable) vs
-         p' = mkLam vs p
+         p' = mkLam vs e
          t' = typeof p'
 go (In (App NoInline _ [p])) = do
   p'' <- go p'
