70f7c4897c5b22c3b76f5084cba06beefb095b62
Josef Svenningsson <josef.svenningsson@gmail.com>
2014-08-22 11:02:14 +0200

    Add the missing MultiDim file that I forgot to include in the previous commit

---
+++ src/Feldspar/Core/Constructs/MultiDim.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Feldspar.Core.Constructs.MultiDim where
+
+import Language.Syntactic
+import Feldspar.Lattice
+import Feldspar.Core.Types
+import Feldspar.Core.Interpretation
+import Feldspar.Core.Constructs.Binding
+
+data MultiDIM a where
+  MDShape   :: Type a => MultiDIM (MultiDim a :-> Full [Length])
+  MDPayload :: Type a => MultiDIM (MultiDim a :-> Full [a])
+
+instance Semantic MultiDIM where
+  semantics MDShape   = Sem "shape" mdDim
+  semantics MDPayload = Sem "arr" mdArr
+
+semanticInstances ''MultiDIM
+
+instance EvalBind MultiDIM where evalBindSym = evalBindSymDefault
+
+instance AlphaEq dom dom dom env => AlphaEq MultiDIM MultiDIM dom env
+  where
+    alphaEqSym = alphaEqSymDefault
+
+instance Sharable MultiDIM
+
+instance Monotonic MultiDIM
+
+instance SizeProp (MultiDIM :|| Type) where
+  sizeProp (C' MDShape) (WrapFull md :* Nil) = len
+    where (len,_) = infoSize md
+  sizeProp (C' MDPayload) (WrapFull md :* Nil) = arr
+    where (_,arr) = infoSize md
+
+instance (SizeProp (MultiDim :|| Type)
+         ,(MultiDim :|| Type) :<: dom
+         ) =>
+         Optimize (MultiDim :|| Type) dom where
+  constructFeatUnOpt opts x@(C' _) = constructFeatUnOptDefault opts x
