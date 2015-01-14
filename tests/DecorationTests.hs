ad74d519a2e53d159d711f1cfe28747ee940828c
Emil Axelsson <emax@chalmers.se>
2015-01-14 10:42:52 +0100

    Added decoration test for code motion

--- tests/DecorationTests.hs
+++ tests/DecorationTests.hs
@@ -29,10 +29,19 @@ monadicSharing a = runMutable $ do
     c' <- getRef c
     return (c'+(b'+3))
 
+-- An example with nested sharing. Our first codeMotion would miss the opportunity to share `(a+b)`.
+trickySharing :: Data Index -> Data Index
+trickySharing x = (a+b+c) + (a+b) + (a+b+c)
+  where
+    a = x*3
+    b = x*5
+    c = x*7
+
 tests = testGroup "DecorationTests"
     [ goldenVsFile "example9" "tests/gold/example9.txt" "tests/example9.txt" $ writeFile "tests/example9.txt" $ showDecor example9
     , goldenVsFile "topLevelConsts" "tests/gold/topLevelConsts.txt" "tests/topLevelConsts.txt" $ writeFile "tests/topLevelConsts.txt" $ showDecor topLevelConsts
     , goldenVsFile "monadicSharing" "tests/gold/monadicSharing.txt" "tests/monadicSharing.txt" $ writeFile "tests/monadicSharing.txt" $ showDecor monadicSharing
+    , goldenVsFile "trickySharing" "tests/gold/trickySharing.txt" "tests/trickySharing.txt" $ writeFile "tests/trickySharing.txt" $ showDecor trickySharing
     ]
 
 main = defaultMain $ testGroup "Tests" [tests]
