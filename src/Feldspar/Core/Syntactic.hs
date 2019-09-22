f9298982dcaf317a8ee16d1dacb41b0320cc9ac8
Karl-Filip Faxén <kff@sics.se>
2019-09-22 21:45:04 +0200

    Preparing Syntactic for incremental CSE.

--- src/Feldspar/Core/Syntactic.hs
+++ src/Feldspar/Core/Syntactic.hs
@@ -13,14 +13,18 @@ module Feldspar.Core.Syntactic
     , drawDecorWith
     , render
     , alphaEq
+    , evalBind
     ) where
 
 #ifndef INCREMENTAL_CSE
 
 import Language.Syntactic
-import Language.Syntactic.Constructs.Binding (alphaEq)
+import Language.Syntactic.Constructs.Binding (alphaEq, evalBind)
 import Language.Syntactic.Constructs.Decoration (showDecorWith, drawDecorWith)
 
 #else
+
+import Feldspar.Core.Reify
+import Feldspar.Core.Render
 
 #endif
