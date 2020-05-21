942f3bcb51eccf60bbaade3cd27352e02db9a352
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2020-05-21 14:52:06 +0200

    Derive Lift instances
    These are boilerplate instances so let
    the compiler derive them.

--- src/Feldspar/Core/UntypedRepresentation.hs
+++ src/Feldspar/Core/UntypedRepresentation.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveLift #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -91,6 +92,7 @@ import Data.List (nub, intercalate)
 import Data.Tree
 import Data.Int
 import Data.Word
+import Language.Haskell.TH.Syntax (Lift(..))
 
 import Feldspar.Core.Representation (VarId(..))
 
@@ -156,10 +158,10 @@ dropAnnotation (AIn _ e) = e
 
 data Size = S8 | S16 | S32 | S40 | S64
           | S128 -- Used by SICS.
-    deriving (Eq,Show,Enum,Ord)
+    deriving (Eq,Show,Enum,Ord,Lift)
 
 data Signedness = Signed | Unsigned
-    deriving (Eq,Show,Enum)
+    deriving (Eq,Show,Enum,Lift)
 
 data Fork = None | Future | Par | Loop
     deriving (Eq,Show)
