07b7e085220d761fe8b8a709c2860a92f3313c2b
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2020-06-29 13:07:59 +0200

    Fix some warnings, NFC

--- src/Feldspar/Onnx/onnxToFeld.hs
+++ src/Feldspar/Onnx/onnxToFeld.hs
@@ -1,3 +1,30 @@
+--
+-- Copyright (c) 2020, ERICSSON AB
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+--     * Redistributions of source code must retain the above copyright notice,
+--       this list of conditions and the following disclaimer.
+--     * Redistributions in binary form must reproduce the above copyright
+--       notice, this list of conditions and the following disclaimer in the
+--       documentation and/or other materials provided with the distribution.
+--     * Neither the name of the ERICSSON AB nor the names of its contributors
+--       may be used to endorse or promote products derived from this software
+--       without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
 {-# OPTIONS_GHC -Wall #-}
 
 module Main where
@@ -19,31 +46,26 @@ import Onnx.TensorShapeProto.Dimension as TSP
 import Onnx.TensorShapeProto.Dimension.Value as TSP
 
 import Text.ProtocolBuffers
-import Text.ProtocolBuffers.Header as H(Utf8(..))
+import Text.ProtocolBuffers.Header as H (Utf8(..))
 
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.UTF8 as U
 
-import qualified Data.Foldable as D(toList)
+import Data.Either (either)
+import qualified Data.Foldable as D (toList)
 import Data.List (intercalate)
+import Data.Maybe (maybe, fromJust, fromMaybe)
+import System.Environment (getArgs)
 
-import System.Environment(getArgs)
-
+main :: IO ()
 main = do args <- getArgs
           let [modelFileName] = take 1 args -- First argument is file name
           -- putStrLn modelFileName
           modelBinary <- L.readFile modelFileName
           -- putStrLn "Read it!"
-          let model = case messageGet modelBinary of
-                         Left msg -> error $ "Error: " ++ msg
-                         Right (m,_) -> m
-          let gr = case graph model of
-                     Nothing -> error $ "No graph in model"
-                     Just g -> g
-          let grName = case G.name gr of
-                         Nothing -> error "This graph has no name"
-                         Just (H.Utf8 n) -> n
-          -- putStrLn $ U.toString grName
+          let model = either (\s -> error $ "Error: " ++ s) fst $ messageGet modelBinary
+              gr = fromMaybe (error "No graph in model") $ graph model
+          -- putStrLn $ maybe (error "Graph has no name") toStr $ G.name gr
           let graphNodes = D.toList $ node gr
           -- putStrLn $ show $ length graphNodes
           -- putStrLn "module Inits where"
@@ -58,15 +80,17 @@ main = do args <- getArgs
           putStrLn "Graph nodes"
           putStr $ unlines $ map showNode graphNodes
 
+showNode :: NodeProto -> String
 showNode n = "  " ++ outs ++ " = " ++ op ++ " " ++ ins ++ "\n" ++ attrs
   where ins = unwords $ map toStr $ D.toList $ N.input n
-        nameStr = toStr $ unJust $ N.name n
-        op = "onnx" ++ (toStr $ unJust $ N.op_type n)
+        -- nameStr = maybe (error "No name string") toStr $ N.name n
+        op = "onnx" ++ toStr (fromJust $ N.op_type n)
         outs = unwords $ map toStr $ D.toList $ N.output n
         attrs = unlines $ map showAttribute $ D.toList $ N.attribute n
 
+showAttribute :: AttributeProto -> String
 showAttribute a = "  -- " ++ nStr ++ " " ++ val nStr
-  where nStr = toStr (unJust $ A.name a)
+  where nStr = toStr (fromJust $ A.name a)
         val "dilations"    = showAttrInts a
         val "group"        = showAttrInt a
         val "kernel_shape" = showAttrInts a
@@ -83,53 +107,56 @@ showAttribute a = "  -- " ++ nStr ++ " " ++ val nStr
         val "transB"       = showAttrInt a
         val _ = "_|_"
 
-showAttrInts  a = intercalate " " $ map show $ D.toList $ ints a
-showAttrInt   a = show $ unJust $ i a
-showAttrFloat a = show $ unJust $ f a
+showAttrInts :: AttributeProto -> String
+showAttrInts  a = unwords $ map show $ D.toList $ ints a
 
-showGI t = toStr (unJust $ TP.name t) ++ " :: " ++ showTensorTypeI (TP.data_type t) (D.toList $ TP.dims t)
-        ++ "\n" ++ toStr (unJust $ TP.name t) ++ " = value [" 
-            ++ intercalate ", " (map show $ D.toList $ TP.float_data t) ++ "]"
+showAttrInt :: AttributeProto -> String
+showAttrInt a = show $ fromJust $ i a
 
-showGIc t | (length $ D.toList $ TP.float_data t) < 100000
-          = showElemT (int2elemT $ unJust $ TP.data_type t) ++ " " ++ toStr (unJust $ TP.name t) ++ "[] = {"
+showAttrFloat :: AttributeProto -> String
+showAttrFloat a = show $ fromJust $ f a
+
+showGI :: TensorProto -> String
+showGI t = toStr (fromJust $ TP.name t) ++ " :: "
+           ++ showTensorType (TP.data_type t) (D.toList $ TP.dims t) ++ "\n"
+           ++ toStr (fromJust $ TP.name t) ++ " = value [" ++ elems ++ "]"
+  where elems = intercalate ", " (map show $ D.toList $ TP.float_data t)
+
+showGIc :: TensorProto -> String
+showGIc t | length (D.toList $ TP.float_data t) < 100000
+          = showElemT (int2elemT $ fromJust $ TP.data_type t) ++ " " ++ toStr (fromJust $ TP.name t) ++ "[] = {"
             ++ intercalate ", " (map show $ D.toList $ TP.float_data t) ++ "};"
           | otherwise
           = "// omitted "
 
--- showTensorType t dims = "Tensor [" ++ unwords sh ++ "] of " ++ show (toEnum $ fromIntegral $ unJust t :: TD.DataType)
---   where sh = map show dims
+showTensorType :: (Integral a, Show b) => Maybe a -> [b] -> String
+showTensorType t dims = "Data [" ++ t' ++ "]" ++ " -- " ++ unwords sh
+  where t' = showElemT (int2elemT $ fromJust t)
+        sh = map show dims
 
-showTensorType t dims = "Data [" ++ showElemT (int2elemT $ unJust t) ++ "]" ++ " -- " ++ intercalate " " sh
-  where sh = map showDim dims
-
-showTensorTypeI t dims = "Data [" ++ showElemT (int2elemT $ unJust t) ++ "]" ++ " -- " ++ intercalate " " sh
-  where sh = map show dims
-
--- showDim (Just (Dim_value i)) = show i
--- showDim (Just (Dim_param s)) = toStr s
--- showDim _ = "*"
-
+int2elemT :: Integral a => a -> DataType
 int2elemT i = toEnum $ fromIntegral i :: TD.DataType
 
+showElemT :: DataType -> String
 showElemT FLOAT = "Float"
 showElemT t = error $ "Type " ++ show t ++ " not implemented"
 
+showVI :: ValueInfoProto -> String
 showVI v = nStr ++ " : " ++ tyStr
-  where nStr = toStr $ unJust $ V.name v
-        tyStr = showType $ unJust $ T.value $ unJust $ V.type' v
+  where nStr = toStr $ fromJust $ V.name v
+        tyStr = showType $ fromJust $ T.value $ fromJust $ V.type' v
 
+showType :: TV.Value -> String
 showType Tensor_type{TV.tensor_type = t}
-   = showTensorType (TT.elem_type t) (map TSP.value $ D.toList $ TSP.dim $ unJust $ TT.shape t)
+  = showTensorType (TT.elem_type t)
+                   (map TSP.value $ D.toList $ TSP.dim $ fromJust $ TT.shape t)
 showType Sequence_type{} = "Sequence"
 showType Map_type{} = "Map"
 
+showDim :: Maybe TSP.Value -> String
 showDim (Just (TSP.Dim_value i)) = show i
 showDim (Just (TSP.Dim_param p)) = toStr p
 showDim Nothing = "*"
-
-unJust (Just x) = x
-unJust Nothing = error $ "unJust on Nothing"
 
 toStr :: H.Utf8 -> String
 toStr (H.Utf8 s) = U.toString s
