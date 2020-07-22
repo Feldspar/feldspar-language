faa9990c792311474b210927566b8d9740914381
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2020-07-22 10:42:58 +0200

    tests: remove topLevelConsts_sics
    The generated code is identical so remove what is now
    a duplicate test.

--- tests/gold/topLevelConsts_sics.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTS_TOPLEVELCONSTS_SICS_H
-#define TESTS_TOPLEVELCONSTS_SICS_H
-
-#include "feldspar_c99.h"
-
-void topLevelConsts__sics(uint32_t v1, uint32_t v2, uint32_t * out);
-
-#endif // TESTS_TOPLEVELCONSTS_SICS_H
